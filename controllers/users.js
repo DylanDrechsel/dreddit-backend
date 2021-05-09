7f669b93ab29a462573374b79efa5ec9415f2710
DylanDrechsel <DrechselDylan@gmail.com>
2021-05-09 14:17:47 -0400

    Completed User Routes and added Comment Schema. All tested through Postman

--- controllers/users.js
+++ controllers/users.js
@@ -2,8 +2,8 @@ import express from 'express';
 import db from '../utils/generatePrisma.js';
 const router = express.Router();
 
-/* SHOW CURRENT LOGGED IN  ROUTE */
-router.get("/verify", async function(request, response){
+// SHOW CURRENT LOGGED IN  ROUTE 
+router.get('/verify', async function(request, response){
     const user = await db.user.findUnique({
         where: {
             id: Number(request.currentUser)
@@ -15,7 +15,15 @@ router.get("/verify", async function(request, response){
 
 // GET ALL USERS
 router.get('/', async (req, res) => {
-		const users = await db.user.findMany({});
+		const users = await db.user.findMany({
+            include: {
+                posts: {
+                    select: {
+                        title: true
+                    }
+                }
+            }
+        });
 		res.json({ users });
 	});
 
@@ -24,6 +32,13 @@ router.get('/:id', async (req, res) => {
     const user = await db.user.findUnique({
         where: {
             id: Number(req.params.id)
+        },
+        include: {
+            posts: {
+                select: {
+                    title: true
+                }
+            }
         }
     })
     res.json({ user });
@@ -31,16 +46,50 @@ router.get('/:id', async (req, res) => {
 
 // CREATE USER ROUTES
 router.post('/create', async (req, res) => {
-    const user = await db.user.create({
-        data: {
-            email: req.body.email,
-            name: req.body.name,
-            age: req.body.age
-        }
+    const createdUser = await db.user.create({
+        data: request.body
     })
-    res.json({ user })
+    res.json({ message: 'The User was created', user: createdUser });
 })
 
+// UPDATE USER
+router.put('/edit', async (req, res) => {
+    const updatedUser = await db.user.update({
+        where: {
+            id: Number(req.currentUser)
+        },
+        data: req.body,
+    });
+    res.json({message: "the User has been updated", user: updatedUser })
+})
+
+// DELETE USER AND ALL THEIR CONTENT
+router.delete('/delete', async (req, res) => {
+    const deleteUserComments = await db.comment.deleteMany({
+        where: {
+            authorId: Number(req.currentUser)
+        }
+    });
+    
+    const deleteUserPosts = await db.post.deleteMany({
+        where: {
+            authorId: Number(req.currentUser)
+        }
+    });
+
+    const deletedUser = await db.user.delete({
+        where: {
+            id: Number(req.currentUser)
+        }
+    })
+
+    res.json({
+			message: 'the user and their posts have been deleted',
+			user: deletedUser,
+			posts: deleteUserPosts,
+			comments: deleteUserComments,
+		});
+})
 
 
 export default router;
