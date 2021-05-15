60dab63629339fa03dcdd91e29d5ccab23fff978
DylanDrechsel <DrechselDylan@gmail.com>
2021-05-15 13:16:52 -0400

    Got image get request to return the actually image instead of the image code

--- server.js
+++ server.js
@@ -1,18 +1,29 @@
 import prisma from '@prisma/client';
-import express from 'express'
-import cors from 'cors'
+import express from 'express';
+import cors from 'cors';
 import userRoutes from './controllers/users.js';
 import postRoutes from './controllers/posts.js';
-import commentRoutes from './controllers/comments.js'
-import likeRoutes from './controllers/likes.js'
-import profileRoutes from './controllers/profile.js'
+import commentRoutes from './controllers/comments.js';
+import likeRoutes from './controllers/likes.js';
+import profileRoutes from './controllers/profile.js';
 import authRequired from './middleware/authRequired.js';
 import { register, login, logout } from './controllers/auth.js';
 // import fileUpload from 'express-fileupload'
-import multer from 'multer'
+import multer from 'multer';
 
-let upload = multer({ dest: 'image'})
-import path from 'path'
+// let upload = multer({ dest: 'image' });
+import path from 'path';
+
+const upload = multer({
+	storage: multer.diskStorage({
+		destination: function (req, file, cb) {
+			cb(null, 'image/');
+		},
+		filename: function (req, file, cb) {
+			cb(null, new Date().valueOf() + '_' + file.originalname);
+		},
+	}),
+});
 
 const db = new prisma.PrismaClient({
 	log: ['info', 'warn'],
@@ -33,16 +44,16 @@ app.use('/register', register);
 app.use('/login', login);
 app.use('/logout', logout);
 app.use('/posts', authRequired, postRoutes);
-app.use('/comments', authRequired, commentRoutes)
+app.use('/comments', authRequired, commentRoutes);
 app.use('/likes', authRequired, likeRoutes);
 app.use('/profile', authRequired, profileRoutes);
 
 app.post('/image', upload.single('image'), async (req, res) => {
 	const image = await db.image.create({
-		data: {...req.file}
-	})
-	res.json({ image })
-})
+		data: { ...req.file },
+	});
+	res.json({ image });
+});
 
 app.get('/image/:filename', (req, res) => {
 	const { filename } = req.params;
@@ -50,7 +61,6 @@ app.get('/image/:filename', (req, res) => {
 	const fullfilepath = path.join(dirname, 'image/' + filename);
 	return res.sendFile(fullfilepath);
 });
-
 
 app.get('/', (req, res) => {
 	res.send('Welcome to SQL');
