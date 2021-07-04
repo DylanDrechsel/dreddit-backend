350c665ae670979e84a9268f1b76b1ff37db2673
DylanDrechsel <DrechselDylan@gmail.com>
2021-07-04 11:31:18 -0400

    Had to add back a different auth because Heroku doesnt let you set cookies

--- middleware/authRequired.js
+++ middleware/authRequired.js
@@ -3,10 +3,8 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const authRequired = (req, res, next) => {
+const tokenAuthRequired = (req, res, next) => {
 	const token = req.cookies.token
-	console.log(req)
-	console.log(req.cookies)
 	console.log(token)
 	// console.log(bearerHeader);
 
@@ -25,4 +23,23 @@ const authRequired = (req, res, next) => {
 	}
 };
 
-export default authRequired;
+const authRequired = (req, res, next) => {
+	const bearerHeader = req.headers['authorization'];
+	// console.log(bearerHeader);
+
+	if (typeof bearerHeader !== 'undefined') {
+		const token = bearerHeader.split(' ')[1];
+		jwt.verify(token, process.env.JWT_SECRET, function (err, payload) {
+			console.log('this is jwt payload', payload);
+
+			if (err) res.sendStatus(500);
+
+			req.currentUser = payload.id;
+			next();
+		});
+	} else {
+		res.sendStatus(403);
+	}
+};
+
+export default tokenAuthRequired; authRequired;
